e433f1700a2247eb78d7cc28d3c291eed4f46062
Akshata069 <akshatabiradar966@gmail.com>
2025-07-18 09:45:22 +0530

    save

---
+++ AddSub.java
@@ -0,0 +1,15 @@
+package login1;
+import java.util.*;
+public class AddSub {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        int c=a+b;
+        int d=a-b;
+        System.out.println("sum of two numbers"+c);
+        System.out.println("sub of two numbers"+d);
+
+    }
+    
+}
